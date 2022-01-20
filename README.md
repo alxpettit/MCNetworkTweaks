dd0b3ac4a0f32561e9c72919ba7b14369ae13561
Alexandria <28266167+alxpettit@users.noreply.github.com>
2022-01-20 09:35:37 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+I made this template for working with my personal mods. If you want a general-purpose one, check out [this](https://github.com/architectury/archloom-example-mod).
+
+- 1.18, Architectory Loom
+- Forge, Fabric-style
+- Everything is GNU GPLv3 because I hate you all :D
---
+++ src/main/java/xyz/achu/modtemplate/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package xyz.achu.modtemplate.mixin;
+
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.achu.modtemplate.ModMain;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        ModMain.LOGGER.info("This line is printed by an example mod mixin!");
+    }
+}
